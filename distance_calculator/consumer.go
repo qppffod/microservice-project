41e448595682e3b6999c6c8fdab9711c95e36bbd
Artur <art67789@gmail.com>
2025-03-26 22:12:09 +0200

    Make kafka consumer

---
+++ distance_calculator/consumer.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/qppffod/microservice-project/types"
+	"github.com/sirupsen/logrus"
+)
+
+type KafkaConsumer struct {
+	consumer  *kafka.Consumer
+	isRunning bool
+}
+
+func NewKafkaConsumer(topic string) (*KafkaConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KafkaConsumer{
+		consumer: c,
+	}, nil
+}
+
+func (c *KafkaConsumer) Start() {
+	logrus.Info("kafka transport started")
+	c.isRunning = true
+	c.readMessageLoop()
+}
+
+func (c *KafkaConsumer) readMessageLoop() {
+	for c.isRunning {
+		msg, err := c.consumer.ReadMessage(-1)
+		if err != nil {
+			logrus.Errorf("kafka consume error: %s", err)
+			continue
+		}
+		var data types.OBUData
+		if err := json.Unmarshal(msg.Value, &data); err != nil {
+			logrus.Errorf("kafka consume corrupt data: %s", err)
+			continue
+		}
+		fmt.Println("NEW_MSG->", msg)
+		fmt.Println("MSG->", data)
+	}
+
+}
