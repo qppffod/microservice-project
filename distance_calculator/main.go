4467fcac5becec85d610132ba021017e71943125
Artur <art67789@gmail.com>
2025-03-26 22:46:18 +0200

    Launch kafka consumer

--- distance_calculator/main.go
+++ distance_calculator/main.go
@@ -3,7 +3,12 @@ package main
 import "log"
 
 func main() {
-	kafkaConsumer, err := NewKafkaConsumer("obudata")
+	var (
+		err error
+		svc CalculatorServicer
+	)
+	svc = NewCalculatorService()
+	kafkaConsumer, err := NewKafkaConsumer("obudata", svc)
 	if err != nil {
 		log.Fatal(err)
 	}
