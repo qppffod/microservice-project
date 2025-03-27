2f24ec7ce18ae6e41d612559162997710de7724b
Artur <art67789@gmail.com>
2025-03-27 19:14:40 +0200

    minor change

--- aggregator/service.go
+++ aggregator/service.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/qppffod/microservice-project/types"
 )
 
@@ -21,6 +19,5 @@ func NewInvoiceAggregator(store Storer) Aggregator {
 }
 
 func (i *InvoiceAggregator) AggregateDistance(distance types.Distance) error {
-	fmt.Println("processing and inserting distance in the storage:", distance)
 	return i.store.Insert(distance)
 }
