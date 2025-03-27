65540f741ecc7653ee647aac25ffbbae7cdeca77
Artur <art67789@gmail.com>
2025-03-27 22:24:13 +0200

    Implement /invoice route

--- aggregator/main.go
+++ aggregator/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/qppffod/microservice-project/types"
 )
@@ -26,7 +27,32 @@ func main() {
 func makeHTTPTransport(listenAddr string, svc Aggregator) {
 	fmt.Println("HTTP transport running on port", listenAddr)
 	http.HandleFunc("/aggregate", handleAggregate(svc))
+	http.HandleFunc("/invoice", handleGetInvoice(svc))
 	http.ListenAndServe(listenAddr, nil)
+}
+
+func handleGetInvoice(svc Aggregator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		values, ok := r.URL.Query()["obu"]
+		if !ok {
+			WriteJSON(w, http.StatusBadRequest, map[string]string{"error": "missing OBU ID"})
+			return
+		}
+
+		obuID, err := strconv.Atoi(values[0])
+		if err != nil {
+			WriteJSON(w, http.StatusBadRequest, map[string]string{"error": "invalid OBU ID"})
+			return
+		}
+
+		invoice, err := svc.CalculateInvoice(obuID)
+		if err != nil {
+			WriteJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+			return
+		}
+
+		WriteJSON(w, http.StatusOK, invoice)
+	}
 }
 
 func handleAggregate(svc Aggregator) http.HandlerFunc {
