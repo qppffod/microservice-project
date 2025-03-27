42668a26d63f0711f72b6adc22f2e474b90e1699
Artur <art67789@gmail.com>
2025-03-27 22:35:59 +0200

    Add CalculateInvoice func to calculate invoice for specific ID

--- aggregator/service.go
+++ aggregator/service.go
@@ -4,8 +4,11 @@ import (
 	"github.com/qppffod/microservice-project/types"
 )
 
+const basePrice = 3.15
+
 type Aggregator interface {
 	AggregateDistance(types.Distance) error
+	CalculateInvoice(int) (*types.Invoice, error)
 }
 
 type InvoiceAggregator struct {
@@ -20,4 +23,19 @@ func NewInvoiceAggregator(store Storer) Aggregator {
 
 func (i *InvoiceAggregator) AggregateDistance(distance types.Distance) error {
 	return i.store.Insert(distance)
+}
+
+func (i *InvoiceAggregator) CalculateInvoice(obuID int) (*types.Invoice, error) {
+	dist, err := i.store.Get(obuID)
+	if err != nil {
+		return nil, err
+	}
+
+	inv := &types.Invoice{
+		OBUID:         obuID,
+		TotalDistance: dist,
+		TotalAmount:   basePrice * dist,
+	}
+
+	return inv, nil
 }
