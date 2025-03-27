1c11531982f440b371e237d31276e2ba68e3ecc5
Artur <art67789@gmail.com>
2025-03-27 19:15:45 +0200

    Add middleware to service

--- aggregator/main.go
+++ aggregator/main.go
@@ -18,6 +18,7 @@ func main() {
 		svc Aggregator
 	)
 	svc = NewInvoiceAggregator(store)
+	svc = NewLogMiddleware(svc)
 
 	makeHTTPTransport(*listenAddr, svc)
 }
@@ -32,8 +33,18 @@ func handleAggregate(svc Aggregator) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var distance types.Distance
 		if err := json.NewDecoder(r.Body).Decode(&distance); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			WriteJSON(w, http.StatusBadRequest, map[string]string{"error": err.Error()})
+			return
+		}
+		if err := svc.AggregateDistance(distance); err != nil {
+			WriteJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
 			return
 		}
 	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
