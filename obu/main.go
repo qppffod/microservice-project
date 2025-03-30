6d1b8e12f1b6ba4ef64af7e9c6eb7c666c495dca
Artur <art67789@gmail.com>
2025-03-30 02:53:27 +0200

    minor change

--- obu/main.go
+++ obu/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math"
 	"math/rand"
 	"time"
 
@@ -51,7 +50,7 @@ func main() {
 func generateOBUIDS(n int) []int {
 	ids := make([]int, n)
 	for i := 0; i < n; i++ {
-		ids[i] = rand.Intn(math.MaxInt)
+		ids[i] = rand.Intn(999999)
 	}
 
 	return ids
