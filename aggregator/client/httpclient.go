467a8872a89114cee268fdd354e63f130bd9f7f7
Artur <art67789@gmail.com>
2025-03-30 02:50:48 +0200

    Implement GetIvoice function

--- aggregator/client/httpclient.go
+++ aggregator/client/httpclient.go
@@ -20,13 +20,46 @@ func NewHTTPClient(endpoint string) *HTTPClient {
 	}
 }
 
+func (c *HTTPClient) GetInvoice(ctx context.Context, id int) (*types.Invoice, error) {
+	invReq := types.GetInvoiceRequest{
+		ObuID: int32(id),
+	}
+	b, err := json.Marshal(&invReq)
+	if err != nil {
+		return nil, err
+	}
+
+	endpoint := fmt.Sprintf("%s/%s?obu=%d", c.Endpoint, "invoice", id)
+	req, err := http.NewRequest("POST", endpoint, bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("the service responded with non 200 status code: %d", resp.StatusCode)
+	}
+
+	var invoice types.Invoice
+	if err := json.NewDecoder(resp.Body).Decode(&invoice); err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return &invoice, nil
+}
+
 func (c *HTTPClient) Aggregate(ctx context.Context, aggReq *types.AggregateRequest) error {
 	b, err := json.Marshal(aggReq)
 	if err != nil {
 		return err
 	}
 
-	req, err := http.NewRequest("POST", c.Endpoint, bytes.NewReader(b))
+	req, err := http.NewRequest("POST", c.Endpoint+"/aggregate", bytes.NewReader(b))
 	if err != nil {
 		return err
 	}
